54e10b165ed7a268a63a66d2d0268ddb681c59ad
Murilo Augusto <muriloact@hotmail.com>
2024-04-15 06:43:56 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import './assets/css/style.css';
+
+let count = 1;
+
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+},5000)
+
+function nextImage(){
+    count++;
+    if(count > 8){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+
+}
+
+const whatsApp = document.querySelector('.whatsappButton');
+
+function whatsAppButton(){
+
+    var phoneNumber = "65999358060";
+    var whatsappLink = "https://wa.me/" + phoneNumber;
+
+
+    return
+}
+whatsAppButton(whatsApp);
+
+document.getElementById("whatsappButton").addEventListener("click", function() {
+    var phoneNumber = "65999358060";
+    
+    var whatsappLink = "https://wa.me/" + phoneNumber;
+    
+    window.location.href = whatsappLink;
+});
